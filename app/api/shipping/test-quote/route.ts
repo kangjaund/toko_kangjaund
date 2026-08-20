e9cb348eb36a1e9dba605ceb988196c57659fafe
kangjaund <kang.jaund@gmail.com>
2026-08-21 00:09:59 +0700

    Add files via upload

---
+++ app/api/shipping/test-quote/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { POST as shippingQuotesPost } from "../quotes/route";
+
+export async function GET() {
+  const request = new Request("http://localhost/api/shipping/quotes", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      destination_id: 73492,
+      items: [
+        {
+          product_id: "658457e1-bd79-4f60-8f57-affeb6e9595c",
+          quantity: 1,
+        },
+      ],
+    }),
+  });
+
+  const response = await shippingQuotesPost(request);
+  const data = await response.json();
+
+  return NextResponse.json(data, { status: response.status });
+}
